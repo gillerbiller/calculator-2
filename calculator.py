bf88b120e7fc37c2be4483b3a04c53461eea3ead
Gillian Hammett <gillerbiller@gmail.com@hackbrightacademy.com>
2020-04-29 23:43:27 +0000

    working calculator code. All functionsfrom arithmetic.py file linked and working in calculator.py

--- calculator.py
+++ calculator.py
@@ -2,6 +2,76 @@
 
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
+def calculator():
+
+     while True:
+        user_input = input("enter your equation > ")
+
+        tokens_from_user_input = user_input.split(' ') 
+
+        if tokens_from_user_input[0] == "q":
+            return False 
+
+        if tokens_from_user_input[0] == "+":
+
+            num1 = int(tokens_from_user_input[1])
+            num2 = int(tokens_from_user_input[2])
+
+            print(add(num1, num2))
+
+        if tokens_from_user_input[0] == "-":
+
+            num1 = int(tokens_from_user_input[1])
+            num2 = int(tokens_from_user_input[2])
+
+            print(subtract(num1, num2))
+        
+        if tokens_from_user_input[0] == "*":
+
+            num1 = int(tokens_from_user_input[1])
+            num2 = int(tokens_from_user_input[2])
+
+            print(multiply(num1, num2))
+
+        if tokens_from_user_input[0] == "/":
+
+            num1 = int(tokens_from_user_input[1])
+            num2 = int(tokens_from_user_input[2])
+
+            print(divide(num1, num2))   
+
+        if tokens_from_user_input[0] == "square":
+
+            num1 = int(tokens_from_user_input[1])
+           
+            print(square(num1)) 
+
+        if tokens_from_user_input[0] == "cube":
+
+            num1 = int(tokens_from_user_input[1])
+            
+            print(cube(num1))
+
+        if tokens_from_user_input[0] == "pow":
+
+            num1 = int(tokens_from_user_input[1])
+            num2 = int(tokens_from_user_input[2])
+
+            print(power(num1, num2))
+
+        if tokens_from_user_input[0] == "mod":
+
+            num1 = int(tokens_from_user_input[1])
+            num2 = int(tokens_from_user_input[2])
+
+            print(mod(num1, num2))
+
+           
+
+calculator()
+       # if tokens_from_user_input[0] == "-":
+
+
 
 
 # Replace this with your code
